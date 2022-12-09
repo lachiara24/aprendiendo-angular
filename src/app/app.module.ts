dd7ddc02791ea68ab4580e5a50d07da35f5a2902
Ludmila Chiara <chiaraludmila24@gmail.com>
2022-12-09 01:17:50 -0300

    Agregado de Header y bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { ContenidoModule } from './contenido/contenido.module';
+import { SharedModule } from './shared/shared.module';
+import { ClientesModule } from './clientes/clientes.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    ContenidoModule,
+    ClientesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contenido/contenido.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InfoPersonalComponent } from './info-personal/info-personal.component';
+
+
+
+@NgModule({
+  declarations: [
+    InfoPersonalComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    InfoPersonalComponent
+  ]
+})
+export class ContenidoModule { }
